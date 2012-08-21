15a65325c6bea869ec481a0ac7e2cecd2596b769
Josh Rolstad <jrolstad@gmail.com>
2012-08-21 13:17:19 -0700

    Adding function to push the contents of an entire directory up to an s3 bucket

---
+++ functions/Push-DirectoryToS3Bucket.ps1
@@ -0,0 +1,46 @@
+﻿<#
+.SYNOPSIS
+Pushes a directory and its children to an S3 bucket
+
+.PARAMETER directoryPath
+Path of the directory to push
+
+.PARAMETER bucketName
+Name of the bucket to push to
+
+.PARAMETER excludeFilter
+Items that should be excluded
+
+.PARAMETER awsAccessKey
+Access key for AWS
+
+.PARAMETER awsSecretkey
+Secret Key for AWS
+
+#>
+function Push-DirectoryToS3Bucket([string] $directory,
+                                [string] $bucketName,
+                                [string] $excludeFilter,
+                                [string] $awsAccessKey = $PoshAws.Preferences.AwsAccessKey,
+                                [string] $awsSecretKey = $PoshAWS.Preferences.AwsSecretKey
+                                )
+{
+    $directoryPath = Resolve-Path $directory
+
+    $filePaths = @{}
+
+    gci $directoryPath -recurse | Where-Object {
+            (Test-Path $_.FullName -PathType Leaf) -eq $true
+        } | ForEach-Object {
+                $fullPath = $_.FullName
+                $name = $_.FullName.Replace($directoryPath,"").TrimStart("\")
+                $filePaths.Add($name,$fullPath)
+            }
+            
+     $filePaths.Keys | ForEach-Object {
+            $key = $_
+            $value = $filePaths[$key]
+            Push-FileToS3Bucket -filePath $value -bucketName $bucketName -s3FileName $key -awsAccessKey $awsAccessKey -awsSecretKey $awsSecretKey
+        }
+
+}
